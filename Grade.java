8f4aacd45d014f06d3a01e54af85bf0ac2bd02b6
prats0111 <prats0111@gmail.com>
2024-10-31 17:58:28 +0530

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,12 @@
+// Class to represent a grade
+public class Grade {
+    String subject;
+    int marks;
+    String grade;
+
+    public Grade(String subject, int marks, String grade) {
+        this.subject = subject;
+        this.marks = marks;
+        this.grade = grade;
+    }
+}
